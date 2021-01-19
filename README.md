95c42cfcd7c8e05f7ea09af1f8b5082dda5bc032
J-P Nurmi <jpnurmi@gmail.com>
2021-01-19 07:01:27 +0100

    Import example app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # flutter_minimal_vkb_example
+
 A minimal virtual keyboard example for Flutter
+
+<a href="https://raw.githubusercontent.com/jpnurmi/flutter_minimal_vkb_example/main/flutter_minimal_vkb_example.gif">
+    <img src="https://raw.githubusercontent.com/jpnurmi/flutter_minimal_vkb_example/main/flutter_minimal_vkb_example.gif" width="250"/>
+</a>
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,189 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(ExampleApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ExampleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Minimal VKB'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: TextField(
+        autofocus: true,
+        controller: _controller,
+        decoration: InputDecoration(
+          suffix: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => _controller.clear(),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomSheet: VirtualKeyboard(),
     );
   }
+}
+
+class VirtualKeyboard extends StatefulWidget {
+  @override
+  VirtualKeyboardState createState() => VirtualKeyboardState();
+}
+
+class VirtualKeyboardState extends State<VirtualKeyboard>
+    implements TextInputSource {
+  // The currently attached client.
+  TextInputClient? _client;
+  // The current text input state.
+  String _text = '';
+  TextSelection _selection = TextSelection.collapsed(offset: 0);
+
+  @override
+  void initState() {
+    super.initState();
+    // Set the virtual keyboard as the current text input source.
+    TextInput.setSource(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // Restore the default text input source.
+    TextInput.setSource(TextInput.defaultSource);
+  }
+
+  // Handle a virtual key button press.
+  void _handleKeyPress(String key) {
+    // Insert text, replacing the current selection if any.
+    _text = _text.replaceRange(_selection.start, _selection.end, key);
+    _selection = TextSelection.collapsed(offset: _selection.start + key.length);
+
+    // Request the attached client to update accordingly.
+    _client?.updateEditingValue(
+      TextEditingValue(text: _text, selection: _selection),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FocusScope(
+      canRequestFocus: false,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          for (final key in ['A', 'B', 'C'])
+            ElevatedButton(
+              child: Text(key),
+              onPressed: _client != null ? () => _handleKeyPress(key) : null,
+            ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  TextInputConnection attach(TextInputClient client) {
+    setState(() => _client = client);
+
+    return VirtualKeyboardConnection(
+      client,
+      onEditingValueSet: (TextEditingValue value) {
+        // Keep track of the currently attached client's editing state changes.
+        _text = value.text;
+        _selection = value.selection;
+      },
+    );
+  }
+
+  @override
+  void detach(TextInputClient client) {
+    setState(() => _client = null);
+  }
+
+  @override
+  void init() {}
+
+  @override
+  void cleanup() {}
+
+  @override
+  void finishAutofillContext({bool shouldSave = true}) {}
+}
+
+class VirtualKeyboardConnection extends TextInputConnection {
+  ValueChanged<TextEditingValue> _onEditingValueSet;
+
+  VirtualKeyboardConnection(
+    TextInputClient client, {
+    required ValueChanged<TextEditingValue> onEditingValueSet,
+  })   : _onEditingValueSet = onEditingValueSet,
+        super(client);
+
+  @override
+  void setEditingState(TextEditingValue value) {
+    _onEditingValueSet(value);
+  }
+
+  @override
+  void setClient(TextInputConfiguration configuration) {}
+
+  @override
+  void updateConfig(TextInputConfiguration configuration) {}
+
+  @override
+  void show() {}
+
+  @override
+  void hide() {}
+
+  @override
+  void clearClient() {}
+
+  @override
+  void setComposingRect(Rect rect) {}
+
+  @override
+  void setEditableSizeAndTransform(Size editableBoxSize, Matrix4 transform) {}
+
+  @override
+  void setStyle({
+    required String? fontFamily,
+    required double? fontSize,
+    required FontWeight? fontWeight,
+    required TextDirection textDirection,
+    required TextAlign textAlign,
+  }) {}
+
+  @override
+  void requestAutofill() {}
+
+  @override
+  void close() {}
+
+  @override
+  void connectionClosedReceived() {}
 }
