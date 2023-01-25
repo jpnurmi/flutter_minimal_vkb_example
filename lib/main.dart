8ed36858025918a42da4f7989d92f1b8646ffbb7
J-P Nurmi <jpnurmi@gmail.com>
2023-01-25 21:50:32 +0100

    Update to Flutter 3.7.0

--- lib/main.dart
+++ lib/main.dart
@@ -93,15 +93,17 @@ class MyVirtualKeyboardState extends State<MyVirtualKeyboard> {
           visible: visible,
           child: FocusScope(
             canRequestFocus: false,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                for (final String key in <String>['A', 'B', 'C'])
-                  ElevatedButton(
-                    child: Text(key),
-                    onPressed: () => _handleKeyPress(key),
-                  ),
-              ],
+            child: TextFieldTapRegion(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  for (final String key in <String>['A', 'B', 'C'])
+                    ElevatedButton(
+                      child: Text(key),
+                      onPressed: () => _handleKeyPress(key),
+                    ),
+                ],
+              ),
             ),
           ),
         );
@@ -110,7 +112,7 @@ class MyVirtualKeyboardState extends State<MyVirtualKeyboard> {
   }
 }
 
-class MyTextInputControl extends TextInputControl {
+class MyTextInputControl with TextInputControl {
   TextEditingValue _editingState = const TextEditingValue();
   final ValueNotifier<bool> _visible = ValueNotifier<bool>(false);
 
